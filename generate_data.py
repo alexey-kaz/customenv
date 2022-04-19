742f96aaf0628f0d83d360beddaad1d1f693e508
alexey-kaz <a.kazantaev@hotmail.com>
2022-04-19 21:40:51 +0300

    specify rcv nodes

--- generate_data.py
+++ generate_data.py
@@ -1,15 +1,15 @@
 import os
-import sys
 import numpy as np
 import pandas as pd
 
 
 class DataGen:
-    def __init__(self, num_agents=5, num_steps=500):
-        self.max_cpu_usage = 5
-        self.max_run_time = 20
+    def __init__(self, rcv_v, num_steps=500, num_agents=5, max_run_time=20):
+        self.rcv_vec = rcv_v
         self.num_agents = num_agents
         self.num_steps = num_steps
+        self.max_run_time = max_run_time
+        self.max_cpu_usage = 5
         self.min_route = 1
         self.max_route = 4
         self.min_queue = 10
@@ -31,7 +31,7 @@ class DataGen:
     def gen_tasks(self):
         tmp_del = 4
         c1 = np.random.randint(0, self.num_steps, size=self.num_steps // tmp_del * self.num_agents)
-        c2 = np.random.randint(0, self.num_agents, size=self.num_steps // tmp_del * self.num_agents)
+        c2 = np.random.choice(self.rcv_vec, size=self.num_steps // tmp_del * self.num_agents)
         c3 = np.random.randint(self.max_run_time, self.num_steps, size=self.num_steps // tmp_del * self.num_agents)
         df = pd.DataFrame({'time': c1, 'rcv': c2, 'life_time': c3, 'life_time_global': c1 + c3})
         df['run_time_vec'] = np.random.randint(1, self.max_run_time,
@@ -53,14 +53,17 @@ class DataGen:
 
 if not os.path.exists('./data'):
     os.makedirs('./data')
-num_agents_vec = [3, 5, 10]
+num_agents_vec = [5, 10, 15]
 num_steps_vec = [250 * 2**i for i in range(4)]
-for i in num_agents_vec:
-    for j in num_steps_vec:
-        datagen = DataGen(i, j)
-        tasks_df = datagen.gen_tasks()
-        relations = datagen.gen_relations()
-        cd_info = datagen.gen_cd_info()
-        tasks_df.to_csv(r'./data/tasks_df_{}_{}.csv'.format(i, j), index=False)
-        cd_info.to_csv(r'./data/cd_info_{}_{}.csv'.format(i, j), index=False)
-        np.save('./data/relations_{}_{}.npy'.format(i, j), relations)
+for i in num_steps_vec:
+    for j in num_agents_vec:
+        for k in np.linspace(j//2, j, 3, dtype=int):
+            print(j, k)
+            rcv_vec = np.random.choice(range(j), k, replace=False)
+            datagen = DataGen(rcv_vec, i, j, i/10)
+            tasks_df = datagen.gen_tasks()
+            relations = datagen.gen_relations()
+            cd_info = datagen.gen_cd_info()
+            tasks_df.to_csv(r'./data/tasks_df_{}_{}_{}.csv'.format(i, j, k), index=False)
+            cd_info.to_csv(r'./data/cd_info_{}_{}_{}.csv'.format(i, j, k), index=False)
+            np.save('./data/relations_{}_{}_{}.npy'.format(i, j, k), relations)
